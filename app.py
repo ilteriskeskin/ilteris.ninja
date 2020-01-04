70460cd07e4cb2eb0a7c5e103dd1b14d54b0f0d6
Ali İlteriş Keskin <aliilteriskeskin@gmail.com>
2020-01-05 00:59:17 +0300

    Edited project

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
-from flask import Flask, render_template, request, jsonify, send_from_directory
+from flask import Flask, render_template, request
+import requests
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def base():
-    return render_template('index.html')
+	if request.method == 'POST':
+		url = 'https://api.github.com/users/' + request.form['data']
+		response = requests.get(url)
+		datas = response.json()
+		datas = dict(datas)
+		return render_template('index.html', datas=datas)
+	else:
+		return render_template('index.html')
