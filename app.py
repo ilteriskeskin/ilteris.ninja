4ccfda26221780bf2364e282f8b8c0f180a105f8
Ali İlteriş Keskin <aliilteriskeskin@gmail.com>
2020-01-07 16:06:55 +0300

    Added find ip address

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 import requests
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'linuxdegilgnulinux'
+
 
 @app.route('/', methods=['GET', 'POST'])
 def base():
+	ip_addr = request.environ.get('HTTP_X_REAL_IP', request.remote_addr)
+	file = open('ip.txt', 'a')
+	file.write(ip_addr)
+	file.close()
+
 	if request.method == 'POST':
 		url = 'https://api.github.com/users/' + request.form['data']
 		response = requests.get(url)
