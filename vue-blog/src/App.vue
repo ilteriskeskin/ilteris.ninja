501a5532c1cb1150b1e33d1a2567e38480f0aeeb
Ali İlteriş Keskin <aliilteriskeskin@gmail.com>
2020-02-09 00:08:04 +0300

    Added vue-router and firebase post, get request example

--- vue-blog/src/App.vue
+++ vue-blog/src/App.vue
@@ -1,60 +1,46 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+  <div id="app" class="container">
+    <div class="row">
+      <div class="col-md-6 col-md-offset-3">
+        <h3>Vue-Resource</h3>
+        <div class="form-group">
+          <input type="text" class="form-control" v-model="userName">
+        </div>
+        <button class="btn btn-primary" @click="saveUser">Save User</button>
+        <button class="btn btn-success" @click="getUsers">Get Users</button>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'app',
-  data () {
+  data() {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      userName: null
     }
-  }
+  },
+  methods: {
+    saveUser() {
+      this.$http.post('https://vuejs-vue-resource-40b21.firebaseio.com/users.json', { userName: this.userName })
+      .then((response) => {
+        console.log(response);
+      })
+    },
+
+    getUsers() {
+      this.$http.get("https://vuejs-vue-resource-40b21.firebaseio.com/users.json")
+      .then(((response) => {
+        console.log(response.data); // response.body alternative
+      }))
+    }
+  },
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
+  margin-top: 100px;
 }
 </style>
--- vue-blog/src/main.js
+++ vue-blog/src/main.js
@@ -1,5 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueResource from "vue-resource"
+
+Vue.use(VueResource);
 
 new Vue({
   el: '#app',
