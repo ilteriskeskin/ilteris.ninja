f2fc163ba218c8759ae8bc2b201c1dfe65be65b6
Ali İlteriş Keskin <aliilteriskeskin@gmail.com>
2020-02-19 15:57:52 +0300

    Edited project

--- vue-blog/src/App.vue
+++ vue-blog/src/App.vue
@@ -1,41 +1,12 @@
 <template>
   <div id="app" class="container">
-    <div class="row">
-      <div class="col-md-6 col-md-offset-3">
-        <h3>Vue-Resource</h3>
-        <div class="form-group">
-          <input type="text" class="form-control" v-model="userName">
-        </div>
-        <button class="btn btn-primary" @click="saveUser">Save User</button>
-        <button class="btn btn-success" @click="getUsers">Get Users</button>
-      </div>
-    </div>
+    Test
   </div>
 </template>
 
 <script>
 export default {
   name: 'app',
-  data() {
-    return {
-      userName: null
-    }
-  },
-  methods: {
-    saveUser() {
-      this.$http.post('https://vuejs-vue-resource-40b21.firebaseio.com/users.json', { userName: this.userName })
-      .then((response) => {
-        console.log(response);
-      })
-    },
-
-    getUsers() {
-      this.$http.get("https://vuejs-vue-resource-40b21.firebaseio.com/users.json")
-      .then(((response) => {
-        console.log(response.data); // response.body alternative
-      }))
-    }
-  },
 }
 </script>
 
--- vue-blog/src/main.js
+++ vue-blog/src/main.js
@@ -1,8 +1,5 @@
 import Vue from 'vue'
 import App from './App.vue'
-import VueResource from "vue-resource"
-
-Vue.use(VueResource);
 
 new Vue({
   el: '#app',
