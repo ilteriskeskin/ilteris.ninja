ac6d46d06b897392d6c102ebc4e7cb68806e5352
Ali İlteriş Keskin <aliilteriskeskin@gmail.com>
2020-02-22 14:02:34 +0300

    Added sidebar

--- vue-blog/src/main.js
+++ vue-blog/src/main.js
@@ -5,14 +5,10 @@ import VueRouter from "vue-router";
 Vue.use(VueRouter);
 
 import Home from './components/Home';
-import Foo from './components/Foo';
-import Bar from './components/Bar';
 
 const router = new VueRouter({
   routes: [
     { path: '/', component: Home, name: 'Home' },
-    { path: '/foo', component: Foo, name: 'Foo' },
-    { path: '/bar', component: Bar, name: 'Bar' },
   ],
   mode: 'history'
 });
