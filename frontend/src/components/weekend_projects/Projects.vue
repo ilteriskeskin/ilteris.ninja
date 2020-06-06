cfcbbccb000eb44b3a4b0f4efebe38ecda0897fe
Ali İlteriş Keskin <aliilteriskeskin@gmail.com>
2020-06-06 20:20:58 +0300

    Added new project

--- frontend/src/components/weekend_projects/Projects.vue
+++ frontend/src/components/weekend_projects/Projects.vue
@@ -3,7 +3,18 @@
     <div class="container">
       <h3 class="postTitle">Hafta sonları yaptığım projeleri bu sayfada yayınlıyorum.</h3>
 
-      <hr />
+      <hr/>
+
+      <h3 class="postTitle">&#128209; Doküman Yazım, Paylaşım Uygulaması</h3>
+      <p
+        class="postPreview"
+      >Doküman oluşturmak için oluşturulmuş, Markdown formatı kullanan web projesi. Proje forklanıp mini bir blog olarak
+        kullanılabilir. Proje giriş yapmadan kullanıldığında local storage'da tutuyor dokümanlarınızı. Eğer giriş
+        yaparsanız Flask ile yazılmış API'a post edip veritabanına yazıyor.</p>
+      <p class="postDate">Paylaşılma Tarihi: 31.05.2020</p>
+      <a href="https://write-docs.ilteris.ninja" target="blank">Projeyi Gör</a>
+
+      <hr/>
 
       <h3 class="postTitle">&#128187; Online Python ve Markdown Editör</h3>
       <p
@@ -12,7 +23,7 @@
       <p class="postDate">Paylaşılma Tarihi: 25.05.2020</p>
       <a href="https://editor.ilteris.ninja" target="blank">Projeyi Gör</a>
 
-      <hr />
+      <hr/>
 
       <h3 class="postTitle">&#127760; Ip Adresini ve O Adrese Bağlı Bilgileri Gör</h3>
       <p
@@ -21,7 +32,7 @@
       <p class="postDate">Paylaşılma Tarihi: 17.05.2020</p>
       <router-link to="/info_ip_addr">Projeyi Gör</router-link>
 
-      <hr />
+      <hr/>
 
       <h3 class="postTitle">&#127909; Rasgele Film Seç</h3>
       <p
@@ -30,7 +41,7 @@
       <p class="postDate">Paylaşılma Tarihi: 16.05.2020</p>
       <router-link to="/random_film">Projeyi Gör</router-link>
 
-      <hr />
+      <hr/>
 
       <h3 class="postTitle">&#128035; Şey Ayracı</h3>
       <p
@@ -38,41 +49,41 @@
       >Zamir olan "şey" kelimesinin yanlış yazımını düzeltmek amacıyla ortaya çıkmıştır.</p>
       <p class="postDate">Paylaşılma Tarihi: 09.05.2020</p>
       <router-link to="/projects/sey-ayraci">Projeyi Gör</router-link>
-      <hr />
+      <hr/>
     </div>
   </div>
 </template>
 
 <script>
-export default {
-  name: "Projects"
-};
+  export default {
+    name: "Projects"
+  };
 </script>
 
 <style scoped>
-hr {
-  border: 1px solid #d1cec9;
-  border-radius: 4px;
-  margin-top: 5%;
-  margin-bottom: 5%;
-}
+  hr {
+    border: 1px solid #d1cec9;
+    border-radius: 4px;
+    margin-top: 5%;
+    margin-bottom: 5%;
+  }
 
-.main {
-  margin-bottom: calc(15% + 10px);
-}
+  .main {
+    margin-bottom: calc(15% + 10px);
+  }
 
-.postTitle {
-  font-weight: bold;
-  margin-bottom: 20px;
-  color: #f2f2f2;
-}
+  .postTitle {
+    font-weight: bold;
+    margin-bottom: 20px;
+    color: #f2f2f2;
+  }
 
-.postPreview {
-  color: #d1cec9;
-}
+  .postPreview {
+    color: #d1cec9;
+  }
 
-.postDate {
-  font-style: italic;
-  color: #d1cec9;
-}
+  .postDate {
+    font-style: italic;
+    color: #d1cec9;
+  }
 </style>
